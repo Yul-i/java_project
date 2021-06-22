0e0e082e9e647b51ec4b468070de043c82285648
Yul-i <55575665+Yul-i@users.noreply.github.com>
2021-06-22 18:05:00 +0900

    Add files via upload

---
+++ day01/src/start/Hello.java
@@ -0,0 +1,14 @@
+package start;//항상 맨 위에는 package이름이 와야한다.
+
+public class Hello {
+
+	public static void main(String[] args) {//class안에 main은 1개만
+		// main 함수에 처리내용을 넣어보자.
+		//System은 컴퓨터를 가리킴.출력.프린트!
+		System.out.println("나는 자동완성 입니다.!!!");
+		System.out.println("환영합니다.");
+		System.out.print("반갑습니다.");
+		//syso, sysout + 컨트롤 + 스페이스바
+	}
+
+}
---
+++ day01/src/start/Hi.java
@@ -0,0 +1,12 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+
+		System.out.println("문장을");
+		System.out.println("3줄");
+		System.out.println("프린터해보세요.");
+	}
+
+}
