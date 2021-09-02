8a2317a6fedf979af5bc5d9b80466f8a536203f1
Yul-i <55575665+Yul-i@users.noreply.github.com>
2021-09-02 18:21:45 +0900

    Add files via upload

---
+++ day52/spring12/src/main/java/com/mega/mvc12/MemberController.java
@@ -0,0 +1,49 @@
+package com.mega.mvc12;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//싱글톤 객체, 스프링에 컨트롤러로 등록
+@Controller
+public class MemberController {
+	
+	@Autowired //자동연결
+	MemberDAO dao; //스프링이 만들어놓은 싱글톤 객체의 주소를 넣어준다.
+	
+	//요청 주소 하나당 메서드 하나
+	@RequestMapping("login.mega")
+	public String login(MemberDTO memberDTO) {
+		System.out.println(memberDTO);
+		System.out.println("싱글톤으로 만들어진 주소 프린트 : "+ dao);
+		boolean result = dao.login(memberDTO);
+		if (result) {
+			return "ok"; //views/ok.jsp
+		}else {
+			return "no"; //vies/no.jsp
+		}
+	}	
+	
+	@RequestMapping("check.mega")
+	public String check(String nick) {
+		System.out.println(nick);
+		boolean result = dao.check(nick);
+		if (result) {
+			return "super";
+		}else {
+			return "common";
+		}
+	}
+	
+	@RequestMapping("password.mega")
+	public String pass(String pass) {
+		System.out.println(pass);
+		if (pass.equals("1234")) {
+			return "pass";
+		}else {
+			return "redirect:index.jsp";
+			//response.sendRedirect("index.jsp");
+		}
+	}
+}
---
+++ day52/spring13/src/main/java/com/mega/mvc13/MemberDAO.java
@@ -0,0 +1,50 @@
+package com.mega.mvc13;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public class MemberDAO {
+	
+	@Autowired
+	SqlSessionTemplate mybatis;
+	
+	public boolean login(MemberDTO dto) {
+		
+		String dbId = "root";
+		String dbPw = "1234";
+		boolean result = false; 
+		
+		if (dto.getId().equals(dbId)&&dto.getPw().equals(dbPw)) {
+			result = true;
+		}
+		return result;
+	}
+	
+	public boolean check(String nick) {
+		String dbNick = "superman";
+		boolean result = false;
+		if (nick.equals(dbNick)) {
+			result = true;
+		}
+		return result;
+	}
+	
+	public void create(MemberDTO memberDTO) {
+		mybatis.insert("member.create", memberDTO);
+		
+	}
+	
+	public void read() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+}
